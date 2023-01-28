a3a8b0f0c7972dfa26d418ade4829ec589b843ed
SQLAdmin <abuyeahia24@gmail.com>
2023-01-29 03:59:53 +0600

    loooppp practice

---
+++ loop_practice.js
@@ -0,0 +1,65 @@
+// ajke amar mon valo nai 39 time write
+
+// var String = "ajke amr mon valo nai";
+// for(i=0; i<=39; i++){
+//     if(i<=39){
+//         console.log(String);
+//     }
+// }
+
+// problem 1
+
+// for(i=58; i<=98; i++){
+//     console.log(i);
+// }
+
+// Problem 2 find odd
+// for(i=581; i<=623; i=i+2){
+//     console.log(i);
+
+// }
+
+
+// problem 3 find even
+// for(i=412; i<=456; i=i+2){
+//     console.log(i);
+
+// }
+
+
+// problem 4 
+
+// var Strings = ['css','html','tailwind','bootstrap'];
+
+
+// for(i=0;i<Strings.length;i++){
+//     var string = Strings[i];
+//     console.log(string);
+//     console.log(string.length);
+    
+    
+
+// }
+
+
+// Problem 4
+// for(i=30; i<=86; i++){
+//     console.log(i);
+//     if(i>=44){
+//         break;
+//     }
+// }
+
+
+// problem 5 
+
+// var booksPrice = [100,140,170,180,200, 267,500];
+// for(i=0; i<booksPrice.length;i++){
+//     var book = booksPrice[i];
+//     if(book>200){
+//         continue;
+//     }
+//     console.log(book);
+// }
+
+
