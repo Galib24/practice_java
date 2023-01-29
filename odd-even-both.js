52b0758ab31ab6124bec1683156a464972e784c1
SQLAdmin <abuyeahia24@gmail.com>
2023-01-29 23:20:33 +0600

    loop and wile using add even

---
+++ odd-even-both.js
@@ -0,0 +1,27 @@
+// using while
+
+let a =1;
+while(a<7){
+    if(a%2==0){
+        console.log(a, 'is even');
+    }
+    else{
+        console.log(a, 'is odd');
+        
+    }
+    a+=1;
+    
+}
+
+// using for llop 
+
+for(a=1;a<7;a+=1){
+    if(a%2==0){
+                console.log(a, 'is even');
+            }
+            else{
+                console.log(a, 'is odd');
+                
+            }
+    
+}
