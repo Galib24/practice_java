a7c2bd4e9eea9b0149af1655586fd4390385dfa4
SQLAdmin <abuyeahia24@gmail.com>
2023-01-30 02:00:58 +0600

    funtion

---
+++ funtion-object.js
@@ -0,0 +1,10 @@
+
+// function declaration
+function startFan(){
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+// call the function
+startFan();
---
+++ perameter.js
@@ -0,0 +1,61 @@
+
+// function functionName(parameters){
+//     // function body
+//     // return
+
+// }
+// var returnValue = functionName(parameter value)
+
+
+
+
+
+
+
+
+// function bringToast(money){
+//     console.log('toast tara tari ano')
+//     console.log('taka dicen: ', money);
+
+// }
+// bringToast(250);
+
+
+// function with sum
+
+// function add(num1,num2){
+//     console.log(num1,num2);
+//     var sum =num1+num2;
+//     // console.log(sum);
+//     return sum;
+// }
+// // add(4, 6); 
+// var total= add(4, 6);
+// // console.log('total', total);
+
+
+
+// function problem operation 1
+
+// function bringToast(money){
+//     var toastPrice =10;
+//     var quantity = money/toastPrice;
+//     return quantity;
+// }
+// var myTaka = 150;
+// var toast = bringToast(myTaka);
+// console.log('Eating toast: ', toast);
+
+
+
+// function problem operation 1
+
+function getmyCgpa(semester1, semester2){
+    const total = semester1 + semester2;
+    const average = total/2;
+    return average;
+}
+var firstGet= 3;
+var secondGet= 3;
+ var myAverage = getmyCgpa(firstGet, secondGet);
+ console.log('I got: ', myAverage);
