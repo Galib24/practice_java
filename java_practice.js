2cc7d4e451b6490b5b354690492c966396b968f0
SQLAdmin <abuyeahia24@gmail.com>
2023-01-30 22:45:39 +0600

    namta of 13 by using function and loop

---
+++ java_practice.js
@@ -0,0 +1,10 @@
+function multiplicationTable(mul){
+    var namta=0
+    for(i=1; i<=10; i++){
+        namta=i*13;
+        console.log(mul+"*"+i+"="+namta);
+
+    }
+
+}
+multiplicationTable(13);
