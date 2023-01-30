7b8e23b6abe76ee137e1b9248c9df8fd23aea494
SQLAdmin <abuyeahia24@gmail.com>
2023-01-30 23:31:11 +0600

    string added by using function

--- java_practice.js
+++ java_practice.js
@@ -1,10 +1,42 @@
-function multiplicationTable(mul){
-    var namta=0
-    for(i=1; i<=10; i++){
-        namta=i*13;
-        console.log(mul+"*"+i+"="+namta);
 
-    }
+// 13 namta
 
-}
-multiplicationTable(13);
+// function multiplicationTable(mul){
+//     var namta=0
+//     for(i=1; i<=10; i++){
+//         namta=i*13;
+//         console.log(mul+"*"+i+"="+namta);
+
+//     }
+
+
+// }
+// multiplicationTable(13);
+
+
+//uppare case to lower case
+
+// function upperCase(UP){
+//    const bigLetter = 'I AM THE PROGRAMMER';
+//    console.log(bigLetter.toLowerCase());
+    
+// }
+// upperCase();
+
+// //lower case to upper case
+
+// function lowerCase(lw){
+//     const bigLetter = 'i am the programmer';
+//     console.log(bigLetter.toUpperCase());
+     
+//  }
+//  lowerCase();
+
+// joint two string by function
+
+// function fullName(){
+//    const str1 = 'Abu';
+//    const str2 = 'Yeahia';
+//    console.log(str1+" "+str2);
+// }
+// fullName();
