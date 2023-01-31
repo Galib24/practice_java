30aa88cc082e181df42f481fe1d4f096c93f29ae
SQLAdmin <abuyeahia24@gmail.com>
2023-01-31 16:59:38 +0600

    find odd sum with function

--- find_oddsum.js
+++ find_oddsum.js
@@ -1,13 +1,47 @@
+// get sum of array
+
+// function getSumOfAnArray(array){
+//     let sum = 0;
+//     for(var i = 0; i < array.length; i++){
+//         const index =i;
+//         const element = array[index];
+//         sum = sum + element;
+//         console.log(index, element, sum);
+//     }
+//     return sum;
+// }
+// const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+
+// getSumOfAnArray(myNumbers)
+
+
+
 function getSumOfAnArray(array){
     let sum = 0;
     for(var i = 0; i < array.length; i++){
         const index =i;
         const element = array[index];
         sum = sum + element;
-        console.log(index, element, sum);
+        // console.log(index, element, sum);
     }
     return sum;
 }
-const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+function getOddNumberOfAnArray(array){
+    const oddNumber = [];
+    for(let i = 0; i<array.length;i++){
+        const index = i;
+        const element = array[index];
+        if(element %2 !==0){
+            console.log(index, element);
+            oddNumber.push(element);
+        }
+        
+    }
+    return oddNumber;
 
-getSumOfAnArray(myNumbers)
+}
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+const oddNumber = getOddNumberOfAnArray(myNumbers);
+console.log(oddNumber);
+const oddNumberSum = getSumOfAnArray(oddNumber);
+console.log('odd number sum', oddNumberSum);
