f51a1c163e2578cc162b73451dacc17398a81004
SQLAdmin <abuyeahia24@gmail.com>
2023-02-05 19:18:28 +0600

    crack problem

---
+++ crack.js
@@ -0,0 +1,27 @@
+// function findMax(arr){
+     
+//       let largest = arr[0];
+//       for(var i = 0; i<arr.length;i++){
+//         if (arr[i]>=largest) {
+//           largest = arr[i];
+          
+          
+//         }
+//       }
+//       return largest;
+      
+//     }
+//     let arr = [1,2,3,4,5];
+//     var another = findMax(arr)
+    // console.log(another);
+
+    //  another problem
+    
+    let number = [-1,2,-3,4,5,6,-7,8,-9,10];
+    for(let i = 0; i<number.length; i++){
+      const element = number[i];
+      console.log(element);
+    }
+
+
+   
